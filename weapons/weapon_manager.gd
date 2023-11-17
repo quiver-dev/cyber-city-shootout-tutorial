c560ba4e2473d82e235ae7abbdae357da99cb063
Joe Bustamante <josephmbustamante@gmail.com>
2023-11-17 13:35:51 -0500

    Video 6 - create a reusable weapon scene and add a raycast

--- weapons/weapon_manager.gd
+++ weapons/weapon_manager.gd
@@ -1,8 +1,15 @@
 extends Node3D
 
 
+var aim_raycast: RayCast3D = null
+
 @onready var animation_player = $arm_rig/AnimationPlayer
 
 
 func _ready() -> void:
 	animation_player.play("Gun1_Idle_NoMovement")
+
+
+func set_aim_raycast(raycast: RayCast3D):
+	aim_raycast = raycast
+	#TODO also make sure to pass this down into our gun
