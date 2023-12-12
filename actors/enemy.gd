bbfcb2ceb1d242e4100b48bc1f3889ef0a0fed64
Joe Bustamante <josephmbustamante@gmail.com>
2023-12-11 20:21:13 -0500

    Video 16 - add an AnimationTree to our enemy, and blend between walk and idle animations

--- actors/enemy.gd
+++ actors/enemy.gd
@@ -4,6 +4,7 @@ extends Actor
 var player: Player
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
+@onready var playback: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +14,11 @@ func _physics_process(delta: float) -> void:
 	direction = global_position.direction_to(nav.get_next_path_position())
 	var target_xform = global_transform.looking_at(player.global_position)
 	global_transform = global_transform.interpolate_with(target_xform, 0.1)
+
+	if velocity.length() > 0:
+		playback.travel("walking")
+	else:
+		playback.travel("idle")
 
 	super(delta)
 
